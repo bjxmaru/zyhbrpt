303f3d4280ad57affae77310f7aa6ff075b2b4cb
bjxmaru <598bieyifeng@163.com>
2018-06-24 21:04:19 +0800

    change to standard inner join  connect symatic

--- f_fa_bal_dep.sql
+++ f_fa_bal_dep.sql
@@ -23,52 +23,91 @@ select
      sum(fa_curr_dep_end_machine) fa_curr_dep_end_machine ,
      sum(fa_curr_dep_end_building) fa_curr_dep_end_building ,
      sum(fa_curr_dep_end_vehicle) fa_curr_dep_end_vehicle ,
-     sum(fa_curr_dep_end) - sum(fa_curr_dep_end_machine) - sum(fa_curr_dep_end_building) - sum(fa_curr_dep_end_vehicle)   fa_curr_dep_end_other    
+     sum(fa_curr_dep_end) - sum(fa_curr_dep_end_machine) - sum(fa_curr_dep_end_building) - sum(fa_curr_dep_end_vehicle)   fa_curr_dep_end_other   , 
+
+    sum(fa_value_add) fa_value_add  ,
+     sum(fa_accu_dep_add) fa_accu_dep_add   , 
+     sum(fa_value_add_purchase_machine) fa_value_add_purchase_machine,
+     sum(fa_value_add_purchase_building) fa_value_add_purchase_building,
+     sum(fa_value_add_purchase_vehicle) fa_value_add_purchase_vehicle,
+     sum(fa_value_add_purchase_other) fa_value_add_purchase_other , 
+
+     sum(fa_value_add_cons_machine)  fa_value_add_cons_machine  ,  
+     sum(fa_value_add_cons_building)  fa_value_add_cons_building  ,  
+     sum(fa_value_add_cons_vehicle)  fa_value_add_cons_vehicle  , 
+     sum(fa_value_add_cons_other)  fa_value_add_cons_other  , 
+
+    sum(fa_value_add_other_machine)  fa_value_add_other_machine  ,  
+     sum(fa_value_add_other_building)  fa_value_add_other_building  ,  
+     sum(fa_value_add_other_vehicle)  fa_value_add_other_vehicle  , 
+     sum(fa_value_add_other_other)  fa_value_add_other_other , 
+
+sum(fa_accu_dep_add_pur_machine) fa_accu_dep_add_pur_machine , 
+     sum(fa_accu_dep_add_pur_building) fa_accu_dep_add_pur_building , 
+     sum(fa_accu_dep_add_pur_vehicle) fa_accu_dep_add_pur_vehicle , 
+     sum(fa_accu_dep_add_pur_other) fa_accu_dep_add_pur_other  , 
+ sum(fa_accu_dep_add_cons_machine)  fa_accu_dep_add_cons_machine,
+     sum(fa_accu_dep_add_cons_building) fa_accu_dep_add_cons_building,
+     sum(fa_accu_dep_add_cons_vehicle) fa_accu_dep_add_cons_vehicle,
+     sum(fa_accu_dep_add_cons_other) fa_accu_dep_add_cons_other ,
+
+sum(fa_accu_dep_add_other_machine)  fa_accu_dep_add_other_machine,
+     sum(fa_accu_dep_add_other_building) fa_accu_dep_add_other_building,
+     sum(fa_accu_dep_add_other_vehicle) fa_accu_dep_add_other_vehicle,
+     sum(fa_accu_dep_add_other_other) fa_accu_dep_add_other_other 
+
+
+  
+
+ 
+                 
+
+
 from 
 (
 select   cc.ASSET_CODE,cc.ASSET_NAME , 
 
       
-  decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-           decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+  decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+           decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, aa.localoriginvalue)) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , aa.localoriginvalue) )  , 0 )
            ) fa_value_begin   ,    
            
                   
-  decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-            decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+  decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+            decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '房屋及建筑物' , aa.localoriginvalue , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '房屋及建筑物' , aa.localoriginvalue , 0 )    ) )  , 0 )
            ) fa_value_begin_building   ,                    
                                  
                   
-    decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-           decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+    decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+           decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '机器设备' , aa.localoriginvalue , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '机器设备' , aa.localoriginvalue , 0 )    ) )  , 0 )
            ) fa_value_begin_machine   ,                 
                   
                                  
-   decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-           decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+   decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+           decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '运输工具' , aa.localoriginvalue , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '运输工具' , aa.localoriginvalue , 0 )    ) )  , 0 )
@@ -77,47 +116,47 @@ select   cc.ASSET_CODE,cc.ASSET_NAME ,
                   
          
             
-  decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-           decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+  decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+           decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, aa.accudep)) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , aa.accudep) )  , 0 )
            ) fa_accu_dep_begin   ,         
            
            
            
-    decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-          decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+    decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+          decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '房屋及建筑物' , aa.accudep , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '房屋及建筑物' , aa.accudep , 0 )    ) )  , 0 )
            ) fa_accu_dep_begin_building   ,                    
                                  
                   
-    decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-          decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+    decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+          decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '机器设备' , aa.accudep , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '机器设备' , aa.accudep , 0 )    ) )  , 0 )
            ) fa_accu_dep_begin_machine   ,                 
                   
                                  
-   decode(  parameter('begin_year') || parameter('begin_month')  , '201801' ,        
-          decode( aa.accyear||aa.period , parameter('begin_year') || parameter('begin_month') , 
+   decode(  parameter('begin_year_param') || parameter('begin_month_param')  , '201801' ,        
+          decode( aa.accyear||aa.period , parameter('begin_year_param') || parameter('begin_month_param') , 
                  decode( aa.newasset_flag, 1 , 0,
                       decode( aa.asset_state ,'reduce' , 0, 
                          decode(   bb.CATE_NAME , '运输工具' , aa.accudep , 0 ) )) , 0 ),
-           decode( aa.accyear||aa.period  , parameter('begin_year') || parameter('begin_month')  , 
+           decode( aa.accyear||aa.period  , parameter('begin_year_param') || parameter('begin_month_param')  , 
                    decode(aa.newasset_flag ,0 , 0  , 1, 0 , 
                         decode(aa.asset_state , 'reduce' , 0 , 
                                  decode(   bb.CATE_NAME , '运输工具' , aa.accudep , 0 )    ) )  , 0 )
@@ -219,14 +258,14 @@ select   cc.ASSET_CODE,cc.ASSET_NAME ,
                   
                 
                   
-  decode(aa.accyear || aa.period, parameter('begin_year') || parameter('begin_month') , 0  , aa.depamount ) fa_curr_dep_end , 
+  decode(aa.accyear || aa.period, parameter('begin_year_param') || parameter('begin_month_param') , 0  , aa.depamount ) fa_curr_dep_end , 
   
   
-  decode(aa.accyear || aa.period, parameter('begin_year') || parameter('begin_month') , 0  , 
+  decode(aa.accyear || aa.period, parameter('begin_year_param') || parameter('begin_month_param') , 0  , 
               decode(   bb.CATE_NAME , '房屋及建筑物'   , aa.depamount , 0 ) ) fa_curr_dep_end_building , 
-  decode(aa.accyear || aa.period, parameter('begin_year') || parameter('begin_month') , 0  , 
+  decode(aa.accyear || aa.period, parameter('begin_year_param') || parameter('begin_month_param') , 0  , 
               decode(   bb.CATE_NAME , '机器设备'   , aa.depamount , 0 ) ) fa_curr_dep_end_machine , 
-  decode(aa.accyear || aa.period, parameter('begin_year') || parameter('begin_month') , 0  , 
+  decode(aa.accyear || aa.period, parameter('begin_year_param') || parameter('begin_month_param') , 0  , 
               decode(   bb.CATE_NAME , '运输工具'   , aa.depamount , 0 ) ) fa_curr_dep_end_vehicle    , 
               
               
@@ -289,7 +328,7 @@ select   cc.ASSET_CODE,cc.ASSET_NAME ,
           decode ( aa.accyear || aa.period  , macro('m_fa_end_year_dep') || macro('m_fa_end_month_dep')  ,  0 , 
                     decode ( aa.newasset_flag  , 1 ,  
                        decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  , 0  ,   
-                              decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  ,0 )   ) )  ,  0  )  fa_value_add_cons_mechine ,
+                              decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  ,0 )   ) )  ,  0  )  fa_value_add_cons_machine ,
                               
   
   
@@ -298,7 +337,7 @@ select   cc.ASSET_CODE,cc.ASSET_NAME ,
           decode ( aa.accyear || aa.period  , macro('m_fa_end_year_dep') || macro('m_fa_end_month_dep')  ,  0 , 
                     decode ( aa.newasset_flag  , 1 ,  
                        decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  , 0  ,   
-                              decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  ,0 )   ) ) )   fa_accu_dep_add_other_machine , 
+                              decode(   bb.CATE_NAME , '机器设备'   , aa.localoriginvalue , 0 )  ,0 )   ) ) )   fa_value_add_other_machine , 
                     
                               
                               
@@ -503,22 +542,20 @@ select   cc.ASSET_CODE,cc.ASSET_NAME ,
        
               
                               
-from  pam_addreducestyle dd ,  fa_card cc , fa_category bb  , fa_cardhistory  aa  , ORG_FINANCEORG xx
-where
-  11=11
-  and dd.dr = 0 
-  and dd.pk_addreducestyle = cc.PK_ADDREDUCESTYLE
-  and cc.dr = 0
-  and cc.asset_code like  parameter('fa_code')
-  and cc.pk_card =  aa.pk_card
-  and bb.dr = 0
-  and bb.pk_category = aa.pk_category_old
-  and aa.laststate_flag = 'N'
-  and aa.dr = 0
-  and aa.accyear||aa.period  <=  macro('m_fa_end_year_dep') || macro('m_fa_end_month_dep')
-  and aa.accyear||aa.period  >=  parameter('begin_year') || parameter('begin_month')
-  and aa.pk_org = xx.PK_FINANCEORG
-  and xx.dr = 0
-  and xx.CODE  like parameter('org_code')
+  from 
+fa_cardhistory aa   inner join  fa_category  bb 
+				    on ( bb.pk_category = aa.pk_category_old 
+				         and bb.dr = 0 
+				         and aa.laststate_flag = 'N'
+				         and aa.dr = 0 
+				         and aa.accyear||aa.period  <=  macro('m_fa_end_year_dep') || macro('m_fa_end_month_dep')
+				         and aa.accyear||aa.period  >=  parameter('begin_year_param') || parameter('begin_month_param')
+				         and aa.pk_org = 
+				             (select xx.PK_FINANCEORG  from ORG_FINANCEORG  xx where xx.dr = 0  and  xx.code = parameter('org_code') ) 
+				        
+                )  inner join fa_card cc   
+            on  (  cc.pk_card =  aa.pk_card   and cc.dr = 0   and cc.asset_code like  parameter('fa_code') ) 
+
+inner join     pam_addreducestyle dd on ( dd.pk_addreducestyle = cc.PK_ADDREDUCESTYLE and dd.dr = 0  ) 
 
 )
